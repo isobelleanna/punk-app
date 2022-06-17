d27f792f70f1d68da83dfd883737253112e313ef
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-06-17 14:51:09 +0100

    generate random beer for tile

--- src/containers/RandomBeer/RandomBeer.jsx
+++ src/containers/RandomBeer/RandomBeer.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RandomBeer.scss";
 import BeerTile from "../../components/BeerTile/BeerTile";
 
-const RandomBeer = ({ randomBeer }) => {
+const RandomBeer = () => {
+  const [randomBeer, setRandomBeer] = useState([]);
+  const getRandomBeer = async () => {
+    const url = "https://api.punkapi.com/v2/beers/random";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setRandomBeer(data);
+  };
   return (
     <div className="random">
+      <button onClick={getRandomBeer}>Get Beer of the Day</button>
       <BeerTile randomBeer={randomBeer} />
     </div>
   );
--- src/containers/TasterMenu/TasterMenu.jsx
+++ src/containers/TasterMenu/TasterMenu.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TasterMenu.scss";
 import MenuCard from "../../components/MenuCard/MenuCard";
 
 const TasterMenu = () => {
+  const [randomArr, setRandomArr] = useState([]);
+
+  const getRandom = async () => {
+    const url = "https://api.punkapi.com/v2/beers/random";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className="taster-menu">
       <MenuCard />
+      <button onClick={getRandom}>Generate Stater</button>
       <MenuCard />
+      <button onClick={getRandom}>Generate Main</button>
       <MenuCard />
+      <button onClick={getRandom}>Generate Generate Dessert</button>
     </div>
   );
 };
